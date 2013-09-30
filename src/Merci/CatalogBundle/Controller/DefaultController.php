85d294397a9341913b8e774c67faddd8fdfd53aa
Aluno <aluno@season.com.br>
2013-09-30 16:22:21 -0300

    Mapeando Objeto com o Banco de Dados

--- src/Merci/CatalogBundle/Controller/DefaultController.php
+++ src/Merci/CatalogBundle/Controller/DefaultController.php
@@ -4,15 +4,15 @@ namespace Merci\CatalogBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class CatalogController extends Controller
+class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('MerciCatalogBundle:Catalog:index.html.twig');
+        return $this->render('MerciCatalogBundle:Default:index.html.twig');
     }
 
     public function productAction()
     {
-        return $this->render('MerciCatalogBundle:Catalog:product.html.twig');
+        return $this->render('MerciCatalogBundle:Default:product.html.twig');
     }
 }
