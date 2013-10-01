8d03b7bdcf5397bcb7583f7b44b88f06106d5003
Aluno <aluno@season.com.br>
2013-09-30 22:55:09 -0300

    Buscando dados no Banco

---
+++ src/Merci/CatalogBundle/Controller/DefaultController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Merci\CatalogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findAll();
+
+        if (empty($products)) {
+            throw $this->createNotFoundException('No products avaiable');
+        }
+
+        return $this->render(
+            'MerciCatalogBundle:Default:index.html.twig',
+            array('products' => $products)
+        );
+    }
+
+    public function productAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findOneById($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found with id: ' . $id);
+        }
+
+        return $this->render(
+            'MerciCatalogBundle:Default:product.html.twig',
+            array('product' => $product)
+        );
+    }
+}
