df277ccf3238f99bf7c9571f085a685487e0f4fa
Aluno <aluno@season.com.br>
2013-10-05 13:52:37 -0300

    Buscando dados com queries

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,8 +15,6 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
-            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
-            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Merci\CatalogBundle\MerciCatalogBundle(),
         );
---
+++ src/Merci/CatalogBundle/Controller/DefaultController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Merci\CatalogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        $products = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findAll();
+
+        if (empty($products)) {
+            throw $this->createNotFoundException('No products avaiable');
+        }
+
+        return $this->render('MerciCatalogBundle:Default:index.html.twig',
+            array('products' => $products)
+        );
+    }
+
+    public function productAction($id)
+    {
+        $product = $this->getDoctrine()
+            ->getRepository('MerciCatalogBundle:Product')
+            ->findOneById($id);
+
+        if (!$product) {
+            throw $this->createNotFoundException('No product found with id: ' . $id);
+        }
+
+        return $this->render('MerciCatalogBundle:Default:product.html.twig',
+            array('product' => $product)
+        );
+    }
+
+    public function searchAction()
+    {
+        $request = $this->getRequest();
+        $find = $request->query->get('find');
+
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery('
+            SELECT p
+            FROM MerciCatalogBundle:Product p
+            WHERE p.name LIKE :find
+        ')->setParameter('find', '%'.$find.'%');
+
+        $products = $query->getResult();
+
+        if (empty($products)) {
+            throw $this->createNotFoundException('No products avaiable');
+        }
+
+        return $this->render('MerciCatalogBundle:Default:index.html.twig',
+            array('products' => $products, 'find' => $find)
+        );
+    }
+}
