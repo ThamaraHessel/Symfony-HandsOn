edgarsandi/symfony_handson

02.B - Criando pagina catalogo

src/Merci/CatalogBundle/Controller/CatalogController.php
@@ -1,0 +1,13 @@
+<?php
+
+namespace Merci\CatalogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CatalogController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('MerciCatalogBundle:Catalog:index.html.twig');
+    }
+}

ThamaraHessel/Symfony-HandsOn

src/Merci/CatalogBundle/Controller/CatalogController.php
@@ -1,0 +1,13 @@
+<?php
+
+namespace Merci\CatalogBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class CatalogController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('MerciCatalogBundle:Default:index.html.twig');
+    }
+}